a44e09408ef1e06972f7ec9745211ad3e2e20c4e
Ravindra Kawale <ravindra.kawale@waiin.com>
2024-05-27 17:30:49 +0530

    Add project files.

---
+++ Coding_Wiki_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coding_Wiki_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
